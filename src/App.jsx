465d5e46161a6a36ebb88f232dc6104f1128733f
Mykola Khomiakov <vkontemail4@gmail.com>
2024-09-12 21:41:50 +0300

    Rename files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import RegisterForm from "./components/RegisterForm";
+import LoginForm from "./components/LoginForm";
+import "./App.sass";
+
+function App() {
+  return (
+    <div className="App">
+      <RegisterForm />
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
